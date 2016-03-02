a79859930c6dd89fc8b7c1e3cc5e3df36036aaed
deka6pb <deka6pb@rambler.ru>
2016-03-02 16:56:52 +0300

    Update YandexObject.php

--- objects/YandexObject.php
+++ objects/YandexObject.php
@@ -1,12 +1,9 @@
 <?php
-
 namespace deka6pb\geocoder\objects;
-
 use deka6pb\geocoder\abstraction\ObjectAbstract;
 use deka6pb\geocoder\Point;
 use yii\helpers\ArrayHelper;
 use yii\helpers\VarDumper;
-
 class YandexObject extends ObjectAbstract
 {
     public function initialization($config = [])
@@ -14,18 +11,18 @@ class YandexObject extends ObjectAbstract
         if (false === is_array($config)) {
             $config = ArrayHelper::toArray($config);
         }
-
         $object = ArrayHelper::getValue($config, 'GeoObject');
-
         $point = ArrayHelper::getValue($object, 'Point.pos', '');
         $point = explode(' ', $point);
-
         $metaData = ArrayHelper::getValue($object, 'metaDataProperty.GeocoderMetaData', []);
+        $area = ArrayHelper::getValue($metaData, 'AddressDetails.Country.AdministrativeArea.SubAdministrativeArea')
+                    ? ArrayHelper::getValue($metaData, 'AddressDetails.Country.AdministrativeArea.SubAdministrativeArea')
+                    : ArrayHelper::getValue($metaData, 'AddressDetails.Country.AdministrativeArea');
 
         // Получаем полный адрес
         $this->address = ArrayHelper::getValue($metaData, 'text');
         // Получаем город
-        $this->data['city'] = ArrayHelper::getValue($metaData, 'AddressDetails.Country.AdministrativeArea.SubAdministrativeArea.Locality.LocalityName');
+        $this->data['city'] = ArrayHelper::getValue($area, 'Locality.LocalityName');
         $this->city = $this->data['city'];
         // Получаем округ/область
         $this->data['area'] = ArrayHelper::getValue($metaData, 'AddressDetails.Country.AdministrativeArea.AdministrativeAreaName');
@@ -34,19 +31,16 @@ class YandexObject extends ObjectAbstract
         // Код страны
         $this->data['countrySlug'] = ArrayHelper::getValue($metaData, 'AddressDetails.Country.CountryCode');
         // Получаем ветку/улицу
-        $this->data['thoroughfare'] = ArrayHelper::getValue($metaData, 'AddressDetails.Country.AdministrativeArea.SubAdministrativeArea.Locality.Thoroughfare.ThoroughfareName');
-        $this->data['street'] = ArrayHelper::getValue($metaData, 'AddressDetails.Country.AdministrativeArea.SubAdministrativeArea.Locality.Thoroughfare.ThoroughfareName');
+        $this->data['thoroughfare'] = ArrayHelper::getValue($area, 'Locality.Thoroughfare.ThoroughfareName');
+        $this->data['street'] = ArrayHelper::getValue($area, 'Locality.Thoroughfare.ThoroughfareName');
         // Получаем номер дома
-        $this->data['house'] = ArrayHelper::getValue($metaData, 'AddressDetails.Country.AdministrativeArea.SubAdministrativeArea.Locality.Thoroughfare.Premise.PremiseNumber');
-
+        $this->data['house'] = ArrayHelper::getValue($area, 'Locality.Thoroughfare.Premise.PremiseNumber');
         $this->locality_type = strripos($this->city, 'деревн') === false
-                                ? self::LOCALITY_TYPE_CITY
-                                : self::LOCALITY_TYPE_VILLAGE;
-
+            ? self::LOCALITY_TYPE_CITY
+            : self::LOCALITY_TYPE_VILLAGE;
         $this->name = ArrayHelper::getValue($object, 'name', '');
         $this->type = ArrayHelper::getValue($metaData, 'kind', '');
         $this->description = ArrayHelper::getValue($object, 'description', '');
-
         $this->point = new Point(['latitude' => $point[1], 'longitude' => $point[0]]);
     }
 }
